9963fbd3cbe009da42f47325884dfb2edfc3d01f
Dmytro Tkachuk <tkachukdimon@gmail.com>
2020-08-01 21:44:03 +0300

    hw-01

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './App.scss';
+import Profile from './components/Profile';
+import user from './components/Profile/user.json';
+import Statistics from './components/Statistics';
+import statisticalData from './components/Statistics/statistical-data.json';
+import FriendList from './components/FriendList';
+import friends from './components/FriendList/friends.json';
+import TransactionHistory from './components/TransactionHistory'
+import transactions from './components/TransactionHistory/transactions.json'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Profile name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats} />
+      <Statistics title="Upload stats" stats={statisticalData}/>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
+
+export default App;
